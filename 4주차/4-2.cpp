b2226e147ac4729db9fec8ced0f1ab7484799938
justinjeong <justin.jeong5@gmail.com>
2020-05-13 15:32:11 +0900

    feat: 4th week
    prob no.1(solved) : prob-W3_P1
    원형배열: circular qrray의 개념이 포함된 문제
    입력의 크기를 N이라 할때 시간복잡도: O(N^2), 공간복잡도: O(N)
    <유사문제>
    https://programmers.co.kr/learn/courses/30/lessons/42891
    
    prob no.2(solved) : prob-W3_P2
    앞선 1번의 문제에서 입력의 크기가 매우커진 형태이다.
    따라서 O(N^2)의 속도로는 풀리지 않는다
    해시맵을 사용하여 탐색 단계에서의 속도를 O(1)으로 하여 속도를 빠르게 하였다.
    입력의 크기를 N이라 할때 시간복잡도: O(N), 공간복잡도: O(N)
    <유사문제>
    https://programmers.co.kr/learn/courses/30/lessons/42891

---
+++ 4주차/4-2.cpp
@@ -0,0 +1,70 @@
+/*
+ *  2020.4.1.
+ *  INHA_problem_solving 4-2
+ */
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <utility>
+
+using namespace std;
+int test_case_amount, input_siz = 0;
+int answer = 0, temp;
+const int VISITED = -1;
+const int UNDISCOVERED = 1;
+
+vector<int> nums;
+unordered_map<int, int> um;
+
+void print_answer() {
+    cout << answer << '\n';
+}
+
+void solution() {
+    for (int idx = 0; idx < input_siz; ++idx) {
+        if (!um.count(nums[idx] + 1)) {
+            um[nums[idx]] = UNDISCOVERED;
+            answer++;
+            continue;
+        }
+        um[nums[idx]] = VISITED;
+    }
+    print_answer();
+}
+
+void preprocess() {
+    nums.resize(input_siz, 0);
+    answer = 0;
+    um.clear();
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> test_case_amount;
+    while (test_case_amount-- != 0) {
+        cin >> input_siz;
+        preprocess();
+
+        for (int input_index = 0; input_index < input_siz; ++input_index) {
+            cin >> temp;
+            nums[input_index] = temp;
+        }
+
+        solution();
+    }
+    return 0;
+}
+
+/*
+ *
+2
+6
+3 4 2 6 5 1
+5
+1 2 3 4 5
+ */
---
+++ 4주차/4-2_failed.cpp
@@ -0,0 +1,69 @@
+/*
+ *  2020.4.1.
+ *  INHA_problem_solving 4-1
+ */
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <utility>
+#include <climits>
+
+using namespace std;
+int test_case_amount, input_siz = 0;
+int answer = 0;
+
+vector<int> nums;
+
+void print_answer() {
+    cout << answer << '\n';
+}
+
+void solution() {
+    for (int out = 0; out < input_siz; ++out) {
+        int target = nums[out];
+        for (int in = out + 1; in < input_siz; ++in) {
+            if (target != nums[in] + 1) continue;
+            target--;
+        }
+        answer++;
+    }
+
+    print_answer();
+}
+
+void preprocess() {
+    nums.resize(input_siz, 0);
+    answer = 0;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> test_case_amount;
+    while (test_case_amount-- != 0) {
+        cin >> input_siz;
+        preprocess();
+
+        for (int input_index = 0; input_index < input_siz; ++input_index) {
+            int temp;
+            cin >> temp;
+            nums[input_index] = temp;
+        }
+
+        solution();
+    }
+    return 0;
+}
+
+/*
+ *
+2
+6
+3 4 2 6 5 1
+5
+1 2 3 4 5
+ */
